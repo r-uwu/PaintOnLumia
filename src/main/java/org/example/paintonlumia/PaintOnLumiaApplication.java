4ae4050423987f2e2816daca48bec118dbed249f
r-uwu <yarrow.uwu@gmail.com>
2026-04-27 15:41:44 +0900

    feat: canvas api 적용, 브레젠험 직선 알고리즘으로 선형보간

---
+++ src/main/java/org/example/paintonlumia/PaintOnLumiaApplication.java
@@ -0,0 +1,13 @@
+package org.example.paintonlumia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintOnLumiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintOnLumiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/paintonlumia/PaintOnLumiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paintonlumia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintOnLumiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
